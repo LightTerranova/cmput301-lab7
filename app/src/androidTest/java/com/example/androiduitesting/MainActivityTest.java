686f62544c256a6c1bab831dc11619b063fe7ea5
LightTerranova <dudiy@ualberta.ca>
2025-10-24 12:26:47 -0600

    adding show activity

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -2,8 +2,7 @@ package com.example.androiduitesting;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static
-        androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
--- app/src/main/java/com/example/androiduitesting/ShowActivity.java
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -1,7 +1,30 @@
 package com.example.androiduitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        TextView contentView = findViewById(R.id.content_view);
+        Button backButton = findViewById(R.id.button_back);
+
+        // Retrieve the clicked city name from intent
+        Intent intent = getIntent();
+        String cityName = intent.getStringExtra("city_name");
+
+        // Display the city name
+        contentView.setText(cityName);
+
+        // Go back to MainActivity
+        backButton.setOnClickListener(v -> finish());
+    }
 }
