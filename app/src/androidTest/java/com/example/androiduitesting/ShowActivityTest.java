15a9951878f111a750079995b05bfab91280c786
LightTerranova <dudiy@ualberta.ca>
2025-10-24 12:27:26 -0600

    adding tests

--- app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
@@ -1,4 +1,18 @@
 package com.example.androiduitesting;
 
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ShowActivityTest {
-}
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario =
+            new ActivityScenarioRule<>(MainActivity.class);
